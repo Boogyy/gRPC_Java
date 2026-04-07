dfa3ab2940ab44b8ebd2695152073cc1045f6c8a
Egor <aovtar74@gmail.com>
2026-04-07 14:13:41 +0300

    Initial local project

---
+++ src/main/java/vk/gRPC/App.java
@@ -0,0 +1,13 @@
+package vk.gRPC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
